26480e5113b4d5645bc49c47f7e40d16b50f308b
TONY65391 <tonyanthony65391@gmail.com>
2025-02-20 15:34:50 -0800

    second commit

--- OMEGA/settings.py
+++ OMEGA/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#acj9q*p8sl=j9)%y6*&_7c)$3s8lx=8!w+93qnwf0xar1tpfj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
