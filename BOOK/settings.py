d527c9aad3cd89817f16098aca0262dd6535956f
TONY65391 <tonyanthony65391@gmail.com>
2025-02-22 16:39:46 -0800

    edit

--- BOOK/settings.py
+++ BOOK/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = '/productionfiles'
+STATIC_ROOT = 'productionfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
